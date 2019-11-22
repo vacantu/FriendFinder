476e7a057841f5b3188ffc07ab1a6684548fbb87
virgiliocantu <virgilio.cantu@gmail.com>
2019-11-21 23:18:06 -0600

    New folder for git, new files.

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on friends-data.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+//var surveyData = require("../data/survey");
+
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    //res.json(surveyData);
+    // I AM NOT SURE WHAT TO DO HERE :()
+  });
+};
+
---
+++ server.js
@@ -0,0 +1,30 @@
+// =========================================
+// SEERVER.JS FOR FRIENDS FINDER
+// VIRGILIO CANTU
+// 11/21/19
+// =========================================
+
+// Dependencies
+// =============================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+
+// Create an Express server
+var app = express();
+// Sets an initi]al port
+var PORT = process.env.EXPRESSPORT || 3000;
+
+// Sets up the Express app to handle data parsing / MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Point to the route files
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
